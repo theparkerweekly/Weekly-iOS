3b8ba748b71f02991e385ca8b84193da06be84b1
MatthewTurk247 <mjturk15@gmail.com>
2020-02-06 08:51:34 -0600

    Rearrange files.

--- Weekly/DetailViewController.swift
+++ Weekly/DetailViewController.swift
@@ -88,16 +88,6 @@ class DetailViewController: UIViewController, StatusBarable, WKNavigationDelegat
 //        super.viewWillDisappear(animated)
 //        navigationController?.hidesBarsOnSwipe = false
 //    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
